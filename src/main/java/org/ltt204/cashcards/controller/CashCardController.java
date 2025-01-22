7ecaf032e40589e87325289bcc44b4d076d9919b
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-01-22 15:50:19 +0700

    Enhance CashCardRepository for extending PagingAndSortingRepository; Implement CashCardController based on written tests

--- src/main/java/org/ltt204/cashcards/controller/CashCardController.java
+++ src/main/java/org/ltt204/cashcards/controller/CashCardController.java
@@ -1,14 +1,17 @@
 package org.ltt204.cashcards.controller;
 
-import org.ltt204.cashcards.models.CashCard;
+import org.ltt204.cashcards.dto.CashCardCreateRequestDto;
+import org.ltt204.cashcards.model.CashCard;
 import org.ltt204.cashcards.repository.CashCardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,7 +19,6 @@ import java.util.Optional;
 public class CashCardController {
     private final CashCardRepository _cashCardRepository;
 
-    @Autowired
     public CashCardController(CashCardRepository cashCardRepository) {
         _cashCardRepository = cashCardRepository;
     }
@@ -27,5 +29,39 @@ public class CashCardController {
         Optional<CashCard> cashCardOptional = _cashCardRepository.findById(cashCardId);
 
         return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> getCashCards(Pageable pageable) {
+        Page<CashCard> page = _cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                )
+        );
+
+        return ResponseEntity.ok(page.getContent());
+    }
+
+
+    /**
+     * Where did UriComponentsBuilder come from?
+     * We were able to add UriComponentsBuilder ucb as a method argument to this POST handler method,
+     * and it was automatically passed in.
+     * How so? It was injected from our now-familiar friend, Spring's IoC Container.
+     * Thanks, Spring Web!
+     **/
+    @PostMapping
+    private ResponseEntity<?> createCashCard(@RequestBody CashCardCreateRequestDto createRequestDto, UriComponentsBuilder ucb) {
+        var newCashCard = _cashCardRepository.save(new CashCard(null, createRequestDto.amount()));
+
+//        return ResponseEntity.created(URI.create("/cashcards/" + newCashCard.id())).build();
+        var locationOfNewCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(newCashCard.id())
+                .toUri();
+
+        return ResponseEntity.created(locationOfNewCard).build();
     }
 }
--- src/main/java/org/ltt204/cashcards/repository/CashCardRepository.java
+++ src/main/java/org/ltt204/cashcards/repository/CashCardRepository.java
@@ -1,7 +1,8 @@
 package org.ltt204.cashcards.repository;
 
-import org.ltt204.cashcards.models.CashCard;
+import org.ltt204.cashcards.model.CashCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
