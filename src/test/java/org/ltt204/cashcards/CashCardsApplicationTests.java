5268ed482bd3ca96126a7a2100c4470acaa4b300
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-01-22 15:49:08 +0700

    red: Create request dto for POST action; Write unit test for get all and create actions

--- src/test/java/org/ltt204/cashcards/CashCardsApplicationTests.java
+++ src/test/java/org/ltt204/cashcards/CashCardsApplicationTests.java
@@ -2,18 +2,29 @@ package org.ltt204.cashcards;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import org.h2.util.json.JSONArray;
 import org.junit.jupiter.api.Test;
+import org.ltt204.cashcards.dto.CashCardCreateRequestDto;
+import org.ltt204.cashcards.model.CashCard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = CashCardsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardsApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(CashCardsApplicationTests.class);
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -36,5 +47,79 @@ class CashCardsApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    /**
+     * Note for DirtiesContext:
+     * Why we need this?
+     * For instance if I do not use this. When run the test will be failed!
+     * (This was said by Spring Academy, Mine still passed : D)
+     * Because we have a Post unit test right below, then the cashCardCount will not be 3 as we expected, but 4 instead
+     * So it's likely clearing the before invoking another unittest for better result.
+     **/
+    @Test
+    @DirtiesContext
+    void shouldReturnCashCardUriWhenDataIsCreated() {
+        double amountForCreateCashCard = 999.99;
+
+        ResponseEntity<Void> response = restTemplate.postForEntity(
+                "/cashcards",
+                new CashCardCreateRequestDto(amountForCreateCashCard),
+                Void.class
+        );
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        HttpHeaders headers = response.getHeaders();
+        System.out.println((headers.getLocation()));
+
+        ResponseEntity<String> responseFromLocationInHeader = restTemplate.getForEntity(headers.getLocation(), String.class);
+
+        DocumentContext documentContext = JsonPath.parse(responseFromLocationInHeader.getBody());
+        assertThat(responseFromLocationInHeader.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Number id = documentContext.read("$.id");
+        Number amount = documentContext.read("$.amount");
+
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(amountForCreateCashCard);
+    }
+
+    @Test
+    void shouldReturnAllCashCardWhenListIsRequired() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=2", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        List<CashCard > page = documentContext.read("$.[*]");
+        assertThat(page.size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldReturnASortedPageOfCashCards() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=2&sort=amount,desc", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        List<CashCard> page = documentContext.read("$[*]");
+        assertThat(page.size()).isEqualTo(2);
+
+        double amount = documentContext.read("$[0].amount");
+        assertThat(amount).isEqualTo(150.01);
+    }
+
+    @Test
+    void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        List<CashCard> page = documentContext.read("$[*]");
+        assertThat(page.size()).isEqualTo(3);
+
+        List<Double> amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactly(1.0, 123.45, 150.01);
     }
 }
